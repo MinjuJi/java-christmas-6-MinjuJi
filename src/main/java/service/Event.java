29de25bc0b18360532135c0b8a2e33bf4e7a744c
Minju Ji <hinote444@gmail.com>
2023-11-13 05:42:44 +0900

    feat(Event): 평일 할인 계산 기능 구현

--- src/main/java/service/Event.java
+++ src/main/java/service/Event.java
@@ -1,10 +1,17 @@
 package service;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Event {
     private static final int MIN_DISCOUNT = 1_000;
     private static final int ADDITIONAL_DISCOUNT = 100;
     private static final int EFFECTIVE_DATE = 1;
     private static final int EXPIRATION_DATE = 25;
+    private static final int EVENT_YEAR = 2023;
+    private static final int EVENT_MONTH = 12;
+    private static final int WEEK_DISCOUNT = 2_023;
+    private static final int NO_DISCOUNT = 0;
 
     public int calculateChristmasDiscountAmount(int reservation) {
         int discountAmount = MIN_DISCOUNT;
@@ -14,6 +21,15 @@ public class Event {
             }
         }
         return discountAmount;
+    }
+
+    public int calculateWeekdayDiscountAmount(int day, int dessertCount) {
+        LocalDate reservation = LocalDate.of(EVENT_YEAR, EVENT_MONTH, day);
+        DayOfWeek dayOfWeek = reservation.getDayOfWeek();
+        if (dayOfWeek != DayOfWeek.FRIDAY && dayOfWeek != DayOfWeek.SATURDAY) {
+            return dessertCount * WEEK_DISCOUNT;
+        }
+        return NO_DISCOUNT;
     }
 
 }
