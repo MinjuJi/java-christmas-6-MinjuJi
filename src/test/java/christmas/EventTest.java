d9cfcedbfe6bc5b2b2fc47d25e5f4c28002582c2
MinjuJi <hinote444@gmail.com>
2023-11-13 18:12:36 +0900

    test(EventTest):크리스마스 디데이 할인 테스트에서 이벤트 기간 이후 할인 금액을 검사하는 테스트 추가

--- src/test/java/christmas/EventTest.java
+++ src/test/java/christmas/EventTest.java
@@ -15,15 +15,23 @@ public class EventTest {
         // given
         final int FIRST = 1;
         final int CHRISTMAS_DAY = 25;
+        final int EXPIRED_DAY = 26;
         final int MIN_DISCOUNT = 1_000;
         final int MAX_DISCOUNT = 3_400;
+        final int NO_DISCOUNT = 0;
         int case1 = FIRST;
         int case2 = CHRISTMAS_DAY;
+        int case3 = EXPIRED_DAY;
+
         // when
         int result1 = event.calculateChristmasDiscountAmount(case1);
         int result2 = event.calculateChristmasDiscountAmount(case2);
+        int result3 = event.calculateChristmasDiscountAmount(case3);
+
         // then
         assertThat(result1).isEqualTo(MIN_DISCOUNT);
         assertThat(result2).isEqualTo(MAX_DISCOUNT);
+        assertThat(result3).isEqualTo(NO_DISCOUNT);
     }
+
 }
