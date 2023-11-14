afa04cb0e519b84d746c60b72dac59da4244e5b6
MinjuJi <hinote444@gmail.com>
2023-11-14 11:20:40 +0900

    feat(OutputView): 증정 메뉴를 출력하는 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import domain.Benefit;
 import domain.Menu;
 import domain.Reservation;
 import java.util.Map;
@@ -7,7 +8,7 @@ import service.Event;
 
 public class OutputView {
     public void printPreviewBenefit(Reservation reservation) {
-        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", reservation.getDay());
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n", reservation.getDay());
     }
 
     public void printReservationMenu(Reservation reservation) {
@@ -22,7 +23,15 @@ public class OutputView {
         System.out.println("<할인 전 총주문 금액>");
         Map<Menu, Integer> order = reservation.getOrder();
         int totalPrice = event.calculateTotalPriceBeforeDiscount(order);
-        System.out.printf("%,d원", totalPrice);
+        System.out.printf("%,d원\n", totalPrice);
+    }
+
+    public void print(Benefit benefit) {
+        System.out.println("<증정 메뉴>");
+        if (benefit.isChampagne()) {
+            System.out.println("샴페인 1개");
+        }
+        System.out.println("없음");
     }
 
 }
