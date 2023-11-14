aa81a469167cfcbadadeac098587993f04ceed2f
MinjuJi <hinote444@gmail.com>
2023-11-14 17:44:49 +0900

    feat(Controller): 고객에게 해당하는 이벤트를 저장하는 컨트롤러 구현

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,17 +1,34 @@
 package controller;
 
+import domain.Badge;
+import domain.Benefit;
+import domain.Menu;
+import domain.Reservation;
+import java.util.Map;
+import service.Event;
 import view.InputView;
 
 public class Controller {
     private final InputView inputView = new InputView();
+    private final Event event = new Event();
+    private Reservation reservation;
+    private Benefit benefit;
+    private Badge badge;
 
     public void run() {
         reserve();
+        applyEvent();
     }
 
     private void reserve() {
-        inputView.readDay();
-        inputView.readMenu();
+        int day = inputView.readDay();
+        Map<Menu, Integer> order = inputView.readMenu();
+        reservation = new Reservation(day, order);
+    }
+
+    private void applyEvent() {
+        benefit = event.initializeBenefit(reservation);
+        badge = event.offerBadgeByTotalBenefit(benefit.calculateTotalDiscount(), benefit.isChampagne());
     }
 
 }
