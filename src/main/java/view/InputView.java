2fc9a8d911fd57422138dd20d469cad957855f60
MinjuJi <hinote444@gmail.com>
2023-11-14 14:19:44 +0900

    refactor(InputView): 메뉴를 입력받는 메소드의 매개변수를 전역변수로 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,6 +9,8 @@ import utils.Utils;
 public class InputView {
     private static final int MENU_INDEX = 0;
     private static final int COUNT_INDEX = 1;
+    private static final String firstDelimiter = "\\s*,\\s*";
+    private static final String secondDelimiter = "\\s*-\\s*";
 
     public int readDay() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
@@ -16,7 +18,7 @@ public class InputView {
         return Integer.parseInt(day);
     }
 
-    public Map<Menu, Integer> readMenu(String firstDelimiter, String secondDelimiter) {
+    public Map<Menu, Integer> readMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         Map<Menu, Integer> order = new HashMap<>();
         String[] separatedOrders = Utils.splitInput(Console.readLine(), firstDelimiter);
