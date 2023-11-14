6f061a0a7a19294d6dde2dca096989207b2b266c
MinjuJi <hinote444@gmail.com>
2023-11-14 11:05:05 +0900

    feat(OutputView): 예약한 주문 메뉴를 출력하는 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,20 @@
 package view;
 
+import domain.Menu;
+import domain.Reservation;
+import java.util.Map;
+
 public class OutputView {
-    public void printPreviewBenefit(int day) {
-        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", day);
+    public void printPreviewBenefit(Reservation reservation) {
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", reservation.getDay());
     }
+
+    public void printReservationMenu(Reservation reservation) {
+        System.out.println("<주문 메뉴>");
+        Map<Menu, Integer> order = reservation.getOrder();
+        for (Map.Entry<Menu, Integer> menuAndCount : order.entrySet()) {
+            System.out.printf("%s %d개", menuAndCount.getKey(), menuAndCount.getValue());
+        }
+    }
+
 }
