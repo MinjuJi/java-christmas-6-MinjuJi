df5d4c70c1f94d8da8424818385d579f48bf47ea
MinjuJi <hinote444@gmail.com>
2023-11-14 11:14:26 +0900

    feat(OutputView): 할인 전 총주문 금액을 출력하는 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,6 +3,7 @@ package view;
 import domain.Menu;
 import domain.Reservation;
 import java.util.Map;
+import service.Event;
 
 public class OutputView {
     public void printPreviewBenefit(Reservation reservation) {
@@ -13,8 +14,15 @@ public class OutputView {
         System.out.println("<주문 메뉴>");
         Map<Menu, Integer> order = reservation.getOrder();
         for (Map.Entry<Menu, Integer> menuAndCount : order.entrySet()) {
-            System.out.printf("%s %d개", menuAndCount.getKey(), menuAndCount.getValue());
+            System.out.printf("%s %d개\n", menuAndCount.getKey(), menuAndCount.getValue());
         }
+    }
+
+    public void printTotalPriceBeforeDiscount(Reservation reservation, Event event) {
+        System.out.println("<할인 전 총주문 금액>");
+        Map<Menu, Integer> order = reservation.getOrder();
+        int totalPrice = event.calculateTotalPriceBeforeDiscount(order);
+        System.out.printf("%,d원", totalPrice);
     }
 
 }
