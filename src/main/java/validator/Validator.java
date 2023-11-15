ccb53c8826bab4135329ecf367a8342708b91c7c
MinjuJi <hinote444@gmail.com>
2023-11-15 14:41:11 +0900

    feat(Validator): 주문 메뉴의 총 개수가 1 이상 20 이하의 숫자인지 검사하는 기능 구현

--- src/main/java/validator/Validator.java
+++ src/main/java/validator/Validator.java
@@ -1,6 +1,7 @@
 package validator;
 
 import domain.Menu;
+import java.util.Map;
 
 public class Validator {
     private static final String ONLY_NUMBER_REGEX = "^[0-9]*$";
@@ -16,7 +17,7 @@ public class Validator {
 
     public static void validateDataInRange(int day) {
         if (day < MIN_DAY || day > MAX_DAY) {
-            throw new IllegalArgumentException("1 이상 31 이하로 입력 해주세요.");
+            throw new IllegalArgumentException("방문 날짜를 1 이상 31 이하로 입력해 주세요.");
         }
     }
 
@@ -24,8 +25,18 @@ public class Validator {
         String menuName = menuAndCount[MENU_INDEX];
         for (Menu menu : Menu.values()) {
             if (!menu.name().equals(menuName)) {
-                throw new IllegalArgumentException("메뉴판에 있는 메뉴를 입력 해주세요.");
+                throw new IllegalArgumentException("메뉴판에 있는 메뉴를 입력해 주세요.");
             }
+        }
+    }
+
+    public static void validateMenuCountInRange(Map<Menu, Integer> order) {
+        int totalMenuCount = 0;
+        for (Map.Entry<Menu, Integer> menuCount : order.entrySet()) {
+            totalMenuCount += menuCount.getValue();
+        }
+        if (totalMenuCount < 1 || totalMenuCount > 20) {
+            throw new IllegalArgumentException("주문 메뉴의 총 개수를 1 이상 20 이하로 입력해 주세요.");
         }
     }
 
