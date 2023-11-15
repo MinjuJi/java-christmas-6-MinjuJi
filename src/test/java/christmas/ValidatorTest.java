d84beb8380f2769c0454a09b038b80ebe45f8000
MinjuJi <hinote444@gmail.com>
2023-11-15 18:21:41 +0900

    test(ValidatorTest): 주문 메뉴의 총 개수가 1 이상 20 이하의 숫자인지 검사하는 기능 테스트 구현

--- src/test/java/christmas/ValidatorTest.java
+++ src/test/java/christmas/ValidatorTest.java
@@ -3,6 +3,9 @@ package christmas;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
+import domain.Menu;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import validator.Validator;
@@ -76,5 +79,39 @@ public class ValidatorTest {
         // then
         assertThat(result1).doesNotThrowAnyException();
         assertThat(result2).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 메뉴의 총 개수가 1 이상 20 이하의 범위가 아니면 예외를 발생시킨다.")
+    @Test
+    void 주문_메뉴_총_개수_테스트() {
+        // given
+        Map<Menu, Integer> case1 = new HashMap<>();
+        case1.put(Menu.크리스마스파스타, 20);
+        Map<Menu, Integer> case2 = new HashMap<>();
+        case2.put(Menu.크리스마스파스타, 1);
+        Map<Menu, Integer> case3 = new HashMap<>();
+        case3.put(Menu.크리스마스파스타, 21);
+        Map<Menu, Integer> case4 = new HashMap<>();
+        case4.put(Menu.크리스마스파스타, 0);
+
+        // when
+        Throwable result1 = catchThrowable(() -> {
+            Validator.validateMenuCountInRange(case1);
+        });
+        Throwable result2 = catchThrowable(() -> {
+            Validator.validateMenuCountInRange(case2);
+        });
+        Throwable result3 = catchThrowable(() -> {
+            Validator.validateMenuCountInRange(case3);
+        });
+        Throwable result4 = catchThrowable(() -> {
+            Validator.validateMenuCountInRange(case4);
+        });
+
+        // then
+        assertThat(result1).doesNotThrowAnyException();
+        assertThat(result2).doesNotThrowAnyException();
+        assertThat(result3).isInstanceOf(IllegalArgumentException.class);
+        assertThat(result4).isInstanceOf(IllegalArgumentException.class);
     }
 }
