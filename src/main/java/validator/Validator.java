c5291086e3470ce0b92541c368c0b0c9bc1d8544
MinjuJi <hinote444@gmail.com>
2023-11-15 12:39:42 +0900

    feat(Validator): 방문할 날짜가 1 이상 31 이하의 범위인지 검사하는 기능 구현

--- src/main/java/validator/Validator.java
+++ src/main/java/validator/Validator.java
@@ -2,6 +2,8 @@ package validator;
 
 public class Validator {
     private static final String ONLY_NUMBER_REGEX = "^[0-9]*$";
+    private static final int MIN_DAY = 1;
+    private static final int MAX_DAY = 31;
 
     public static void validateInputIsNumeric(String input) {
         if (!input.matches(ONLY_NUMBER_REGEX)) {
@@ -9,4 +11,9 @@ public class Validator {
         }
     }
 
+    public static void validateDataInRange(int day) {
+        if (day < MIN_DAY || day > MAX_DAY) {
+            throw new IllegalArgumentException("1 이상 31 이하로 입력 해주세요.");
+        }
+    }
 }
